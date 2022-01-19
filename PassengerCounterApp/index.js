6a5cfd2679ec2e418ad68d622fd295995c105063
shailijha <shaili_jha@yahoo.co.in>
2022-01-19 15:29:19 -0600

    Save function

--- PassengerCounterApp/index.js
+++ PassengerCounterApp/index.js
@@ -1,8 +1,15 @@
 let count = 0;
 let countElement = document.getElementById("count-el");
-console.log(countElement);
+let saveElement = document.getElementById("save-el");
 
 function increment() {
     count += 1;
-    countElement.innerText = count;    
+    countElement.textContent = count;    
+}
+
+function save() {
+    let textToRender = count + " - ";
+    saveElement.textContent += textToRender;
+    count = 0;
+    countElement.textContent = count;
 }
