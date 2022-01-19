db34e4f61816cc03be6c09058a10a81742e4c7f3
shailijha <shaili_jha@yahoo.co.in>
2022-01-19 12:11:08 -0600

    Passenger counter app

---
+++ PassengerCounterApp/index.js
@@ -0,0 +1,12 @@
+let myAge = 29;
+let humanDogRatio = 7;
+let myDogAge = myAge * humanDogRatio;
+console.log(myDogAge);
+
+let bonusPoints = 50;
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
