ab680d8397c377bea2d9edf7a3fda985a29bae1f
shailijha <shaili_jha@yahoo.co.in>
2022-01-23 15:24:56 -0600

    Added start game button

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,4 +1,20 @@
-let firstCard = 5;
-let secondCard = 9;
+let firstCard = 11;
+let secondCard = 11;
 let sum = firstCard + secondCard;
-console.log(sum);
+let hasBlackJack = false;
+let isAlive = true;
+let message = ''
+let messageEl = document.getElementById("message-el");
+
+function startGame() {
+    if(sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if(sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
+    messageEl.textContent = message;
+}
