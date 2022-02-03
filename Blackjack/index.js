c38bfd4cd2286f32eda04ffe4fa938657e2ffe0d
shailijha <shaili_jha@yahoo.co.in>
2022-02-02 21:08:13 -0600

    Display sum of cards and cards

--- Blackjack/index.js
+++ Blackjack/index.js
@@ -1,12 +1,17 @@
-let firstCard = 11;
-let secondCard = 11;
+let firstCard = 5;
+let secondCard = 9;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = ''
 let messageEl = document.getElementById("message-el");
+// let sumEl = document.getElementById("sum-el");
+let sumEl = document.querySelector("#sum-el");
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
+    cardsEl.textContent += firstCard + " " + secondCard;
+    sumEl.textContent += sum;
     if(sum <= 20) {
         message = "Do you want to draw a new card?";
     } else if(sum === 21) {
